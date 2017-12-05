26821ebce7887d2b7c4594b020856d33a572a8de
victor <xdartxvictor@gmail.com>
2017-12-05 03:00:56 -0500

    Step 3

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -10,18 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var board: UIView!
-    var tileWidth : Int = 0
+    var tileWidth : CGFloat = 0.0
+    var tileCenterX : CGFloat = 0.0
+    var tileCenterY : CGFloat = 0.0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let boardWidth = Int(self.board.frame.width) // ancho del tablero
+        
+        let boardWidth = self.board.frame.width // ancho del tablero
         self.tileWidth = boardWidth / 4   // ancho de cada cuadro para que puedan entrar 4 cuadros por cada fila
-        // Crear cuadros
-        let tileFrame : CGRect = CGRect(x: 0, y: 0, width: self.tileWidth, height: self.tileWidth)
-        let tile : UILabel = UILabel(frame: tileFrame)
-        tile.backgroundColor = UIColor.red
-        self.board.addSubview(tile)
+        
+        self.tileCenterX = self.tileWidth / 2
+        self.tileCenterY = self.tileWidth / 2
+        
+        for _ in 0..<4{
+            // Crear cuadros
+            let tileFrame : CGRect = CGRect(x: 0, y: 0, width: self.tileWidth - 4, height: self.tileWidth - 4)
+            let tile : UILabel = UILabel(frame: tileFrame)
+            tile.center = CGPoint(x: self.tileCenterX, y: self.tileCenterY)
+            tile.backgroundColor = UIColor.red
+            self.board.addSubview(tile)
+            self.tileCenterX = self.tileCenterX + self.tileWidth
+        }
     }
 }
 
